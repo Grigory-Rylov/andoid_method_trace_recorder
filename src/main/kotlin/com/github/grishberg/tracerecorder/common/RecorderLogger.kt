b38898cd85632053bcb710ea3a16b4e4a7489356
Grigory Rylov <grishberg@yandex-team.ru>
2020-05-27 00:12:49 +0300

    v1.4.9 bump ddmlib 26.6.3

--- src/main/kotlin/com/github/grishberg/tracerecorder/common/RecorderLogger.kt
+++ src/main/kotlin/com/github/grishberg/tracerecorder/common/RecorderLogger.kt
@@ -5,4 +5,9 @@ interface RecorderLogger {
     fun e(msg: String)
     fun e(msg: String, t: Throwable)
     fun w(msg: String)
+
+    fun w(tag: String, msg: String) = w("$tag: $msg")
+    fun d(tag: String, msg: String) = d("$tag: $msg")
+    fun e(tag: String, msg: String, t: Throwable) = e("$tag: $msg", t)
+    fun e(tag: String, msg: String) = e("$tag: $msg")
 }
